42f9098546a1a0a3bc1b0599ab32998cce406c42
kwist <kwist879@gmail.com>
2023-06-29 09:32:48 +0300

    updated dist

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/vue-demo/',
+  base: '/eshop/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
